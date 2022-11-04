6052682f913c2bbc83e9d6df9cc13ba1890d6112
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2022-11-04 23:20:08 +0330

    Display Owner Record (HTML) closes #32 Implement Find Owner closes #33 Add ability to add or update an owner closes #34 Add ability to add or update a Pet closes #35 Add ability to add or update a Visit  closes #36 Update Profiles to use Spring Data JPA Services closes #37 Implement WRO4J maven plugin and SPC resources  closes #39 Create Spring MockMVC Tests for Visit Controller  closes #42 Implement Vets List View  closes #43 Date validation error when try persist new pet  closes #45 Write CRUD Tests for Pet Map Service  closes #46 Add Spring Data JPA Repositories closes #47 implement vets list view closes #48 implement vets list view closes #52 add validation for owners, pets and visits  closes #54 implement custom pet validator closes #55

--- src/test/java/com/example/pet/clinic/controller/VisitControllerTest.java
+++ src/test/java/com/example/pet/clinic/controller/VisitControllerTest.java
@@ -30,6 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @ExtendWith(MockitoExtension.class)
 class VisitControllerTest {
 
